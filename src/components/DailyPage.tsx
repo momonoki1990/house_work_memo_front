0b970146dba4d1abdc7d6f289684c04bb3c1556d
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-07-22 11:34:24 +0900

    [add]トップページの追加

---
+++ src/components/DailyPage.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DailyPage extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>DailyPage</div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default DailyPage;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import createStore from './Store'
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
 
+const history = createBrowserHistory();
+const store = createStore(history);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
