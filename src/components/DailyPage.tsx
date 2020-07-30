e8a7247bd5024ba951eaee4094bbf91dd009b3aa
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-07-30 14:38:36 +0900

    [add]レスポンシブ対応(Dailyページ)

--- src/components/DailyPage.tsx
+++ src/components/DailyPage.tsx
@@ -1,30 +1,34 @@
 import React, { useEffect, MouseEvent } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { format } from 'date-fns';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import { fetchDailyWorks, deleteWorkOfDaily, DailyActions } from '../actions';
-
 
 const useStyles = makeStyles(theme => ({
   root: {
   },
   detail: {
-    padding: '4rem 2rem 2rem 2rem',
+    [theme.breakpoints.down('xs')]: {
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1)
+    
+    }
   },
-  table: {
+  grid_row: {
+    textAlign: 'center',
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    borderBottom: '1px solid #E0E0E0'
   }
 }));
 
@@ -34,6 +38,11 @@ const worksOfDailySelector = (state: any) => state.daily.works;
 const monthOfDailySelector = (state: any) => state.daily.month;
 
 const DailyPage = () => {
+
+  // メディアクエリ
+  const theme = useTheme();
+  const window_over_sm = useMediaQuery(theme.breakpoints.up('sm'));
+
   const classes = useStyles();
 
   // グローバルstate
@@ -70,35 +79,37 @@ const DailyPage = () => {
 
   // worksリストの中身
   let listItem = works_of_daily.map((work: any) => (
-    <TableRow key={work.id}>
-      <TableCell component='th' scope='row'>{format(new Date(work.done_date), 'yyyy年MM月dd日')}</TableCell>
-      <TableCell align='center'>{work.Category.name}</TableCell>
-      <TableCell align='center'>{work.note}</TableCell>
-      <TableCell align='center'>{work.done_hours}時間</TableCell>
-      <TableCell align='right'>
-          <Button variant="contained" color="secondary" data-key={work.id} onClick={handleDelete} startIcon={<DeleteIcon />}>削除</Button>
-      </TableCell>
-    </TableRow>
+    <Grid container key={work.id} justify='space-around' className={classes.grid_row}>
+      <Grid item xs={12} sm={2} >{format(new Date(work.done_date), 'yyyy年MM月dd日')}</Grid>
+      <Grid item xs={2} sm={2}>{work.Category.name}</Grid>
+      <Grid item xs={5} sm={2}>{work.note}</Grid>
+      <Grid item xs={3} sm={2}>{work.done_hours}時間</Grid>
+      <Grid item xs={2} sm={2}>
+        {window_over_sm ?
+          (<Button variant="contained" color="secondary" data-key={work.id} onClick={handleDelete} startIcon={<DeleteIcon />}>削除</Button>) :
+          (<DeleteIcon color='secondary' />)
+        }
+      </Grid>
+    </Grid>
   ));
 
   return (
     <div className={classes.root}>
       <Container className={classes.detail} maxWidth='lg'>
-        <Typography variant='h5'><Box fontWeight='fontWeightBold' style={{ borderBottom: '2px solid #f37053' }}>日別詳細</Box></Typography>
+        <Typography variant='h5'><Box fontWeight='fontWeightBold' mt={6} style={{ borderBottom: '2px solid #f37053' }}>日別詳細</Box></Typography>
         <Box display='flex' justifyContent='space-between' mt={2}>
           <Button variant='contained' onClick={handleSubMonth}><ArrowBackIosIcon /></Button>
           <Typography variant='h6'>{format(month_of_daily, 'yyyy年MM月')}</Typography>
           <Button variant='contained' onClick={handleAddMonth}><ArrowForwardIosIcon /></Button>
         </Box>
-        <Box mt={2} p={3} style={{ backgroundColor: '#f6f6f6' }}>
-          <TableContainer component={Paper}>
-            <Table className={classes.table}>
-              <TableBody>
-                {listItem}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Box>
+        {window_over_sm ? (
+          <Box mt={2} p={3} style={{ backgroundColor: '#f6f6f6' }}>
+            <Paper>
+              {listItem}
+            </Paper>
+          </Box>
+        ) : listItem
+        }
       </Container>
     </div>
   )
